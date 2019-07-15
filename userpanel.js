308b9c4cb324cd804c9fdec581b1fb438a521503
nitish kumar <35078711+nitishbeangate@users.noreply.github.com>
2019-07-16 00:01:43 +0530

    Update userpanel.js

--- userpanel.js
+++ userpanel.js
@@ -10,3 +10,29 @@
       $('#'+a+'1').fadeIn();
       
     })
+<input list="browsers" name="browser" class="form-control " id="searchcity">
+  <datalist id="browsers">
+    <option value="Internet Explorer">
+    <option value="Firefox">
+    <option value="Chrome">
+    <option value="Opera">
+    <option value="Safari">
+  </datalist>
+  $("nexttopayment").click(function(){
+    	var seatno=[];
+        var pname=[];
+        var pgender=[];
+        var p_age=[];
+        $('.seatno').each(function(){
+        	   seatno.push($(this).val());
+        	  });
+        	  $('.pname').each(function(){
+        	   pname.push($(this).val());
+        	  });
+        	  $('.pgender').each(function(){
+        	   pgender.push($(this).val());
+        	  });
+        	  $('.p_age').each(function(){
+           	   p_age.push($(this).val());
+           	  });
+    });
